0c894f757cc91892e36be5fa5dafa35fe8e029af
Hassan Mahmood <ai.with.hassan@gmail.com>
2025-04-19 11:29:56 +0200

    Update brain_of_the_doctor.py
    Update model name as previous one is deprecated

--- brain_of_the_doctor.py
+++ brain_of_the_doctor.py
@@ -21,7 +21,8 @@ def encode_image(image_path):
 from groq import Groq
 
 query="Is there something wrong with my face?"
-model="llama-3.2-90b-vision-preview"
+model = "meta-llama/llama-4-scout-17b-16e-instruct"
+#model="llama-3.2-90b-vision-preview" #Deprecated
 
 def analyze_image_with_query(query, model, encoded_image):
     client=Groq()  
