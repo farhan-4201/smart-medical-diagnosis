aec3f21f180a3a44c42005e86d08c98f59aec2f7
Hassan Mahmood <ai.with.hassan@gmail.com>
2025-06-20 23:34:32 +0200

    updated files

--- brain_of_the_doctor.py
+++ brain_of_the_doctor.py
@@ -21,7 +21,9 @@ def encode_image(image_path):
 from groq import Groq
 
 query="Is there something wrong with my face?"
-model = "meta-llama/llama-4-scout-17b-16e-instruct"
+#model = "meta-llama/llama-4-maverick-17b-128e-instruct"
+model="meta-llama/llama-4-scout-17b-16e-instruct"
+#model = "meta-llama/llama-4-scout-17b-16e-instruct"
 #model="llama-3.2-90b-vision-preview" #Deprecated
 
 def analyze_image_with_query(query, model, encoded_image):
